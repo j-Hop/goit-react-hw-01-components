3d44609071169c0bbdefd01e35d76b9205bf2dc8
j-Hop <119629763+j-Hop@users.noreply.github.com>
2023-06-25 12:28:46 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode basename="goit-react-hw-01-components">
     <App />
   </React.StrictMode>
 );
